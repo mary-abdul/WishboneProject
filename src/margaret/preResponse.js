16fa9fc4e2a9961ee5480d57b54de523c515d9be
jiminkim23 <119512552+jiminkim23@users.noreply.github.com>
2023-02-27 19:36:17 +0000

    created preResponse as component

---
+++ src/margaret/preResponse.js
@@ -0,0 +1,21 @@
+import './stylesheet.css'
+
+const preResponse = () => {
+    return(
+    <div>
+    <h1>🌼 🌼 🌼 DAISY WISHES 🌼 🌼 🌼</h1>
+    <p><span>What is your favorite sweet?</span></p>
+    <div class="images">
+      <figure>
+        <img src="1.png"/>
+        <figcaption>chocolate</figcaption>
+      </figure>
+      <figure>
+        <img src="2.png"/>
+        <figcaption>candy</figcaption>
+      </figure>
+    </div>
+    </div>
+    )
+}
+export default preResponse;
