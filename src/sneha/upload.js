8f72a5f384ca0a057b0d842d63b649a7666ab522
jiminkim23 <119512552+jiminkim23@users.noreply.github.com>
2023-02-27 20:00:23 +0000

    upload js file

---
+++ src/sneha/upload.js
@@ -0,0 +1,25 @@
+import './upload.css'
+
+const upload = (props) => {
+    return (
+        <div>
+            <h1>🌼 🌼 🌼 DAISY WISHES 🌼 🌼 🌼</h1>
+            <p><span>Insert question</span></p>
+            <div class="images">
+                <figure>
+                    <button>
+                        <img src="box+document+outline+share+top+upload+icon-1320195323221671611.png" />
+                    </button>
+                    <figcaption>Insert option 1</figcaption>
+                </figure>
+                <figure>
+                    <button>
+                        <img src="box+document+outline+share+top+upload+icon-1320195323221671611.png" />
+                    </button>
+                    <figcaption>Insert option 2</figcaption>
+                </figure>
+            </div>
+        </div>
+    )
+}
+export default upload;
