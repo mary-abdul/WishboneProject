34c2f1eb18efad8abafe0fddd229518cf948c882
jiminkim23 <119512552+jiminkim23@users.noreply.github.com>
2023-02-27 20:33:45 +0000

    added googlelogin

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { GoogleOAuthProvider } from '@react-oauth/google';
-import App from './App';
+import App from './App'
+import googleLogin from './sneha/googleLogin';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <GoogleOAuthProvider clientId="741017505698-4h94mgscj6acm22q74p8tvu2kr62ln9b.apps.googleusercontent.com">
         <React.StrictMode>
+            <googleLogin />
             <App />
         </React.StrictMode>
     </GoogleOAuthProvider>)
