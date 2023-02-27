ee97110f14b61ee651615be0b0672b5257e112fa
jiminkim23 <119512552+jiminkim23@users.noreply.github.com>
2023-02-27 15:21:17 -0500

    create google login

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <GoogleOAuthProvider clientId="741017505698-4h94mgscj6acm22q74p8tvu2kr62ln9b.apps.googleusercontent.com">
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </GoogleOAuthProvider>)
+;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//741017505698-4h94mgscj6acm22q74p8tvu2kr62ln9b.apps.googleusercontent.com
+
+//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
