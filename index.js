000a8bcc70c05c69358b5c38bc6cddef38081980
em <86720055+artemisiaexists@users.noreply.github.com>
2023-03-01 09:53:58 -0500

    Update index.js

--- index.js
+++ index.js
@@ -14,19 +14,41 @@ app.use(express.static('build'))
 
 app.use(express.json())
 app.get("/api/data", (req, res) => {
-  res.json(data)
+  const date = new Date().toISOString().substring(0, 10)
+  res.json(data.find(data => data.id === date))
 })
 
 app.post("/api/data", (req, res) => {
-  let vote = req.body.vote;
-  let user = req.body.user;
+  // req is the request from the user, req.body is what they send
+  // the frontend app send a simple object: { vote: # }
+  // # is 0 or 1
+  const body = req.body
+  console.log(body)
 
-  // logic updating data w/ vote if user hasn't voted
+  // the user's vote
+  const vote = body.vote
+  // today's date
+  const date = new Date().toISOString().substring(0, 10)
+  // find the data for today
+  const dat = data.find(data => data.id === date)
   
+  if(vote === 0) {
+     dat.voting1++
+  } else if(vote === 1) {
+     dat.voting2++
+  } else {
+    console.log("oh no")
+  }
+  
+  // we update the json file--this doesn't actually do much because we can't overwrite our json file on github
   fs.writeFile(
     './data.json',
-    JSON.stringify({notes: data}, null, 2))
-  res.json(data)
+    JSON.stringify(data, null, 2),
+    (err) => {console.log(err)})
+
+  // and this sends today's updated data to the user
+  res.json(dat)
 })
 
-app.listen(3000)
+const PORT = process.env.PORT || 3000
+app.listen(PORT)
